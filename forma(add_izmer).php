d2a00c421f26a43eaa417c85d29e7a09788c01ae
romanov0303 <romanov0303@yandex.ru>
2015-03-22 19:42:30 +0300

    Create forma(add_izmer).php

---
+++ forma(add_izmer).php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+	<title>Form</title>
+	</head>
+	<body>
+
+    <?PHP>
+    <form method="post" action="base(add_edizmer).php">
+	       <p>Name edinici:<br /><input type="text" name="Name_marka" /></p>
+	<input type="submit">
+    </PHP>
+	</body>
+	</html>
